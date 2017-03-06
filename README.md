dc4a750e7531d178f6856a60b669f079572a3d71
Hugo Carneiro <hcarneiro@weboo.biz>
2017-03-06 15:24:47 +0000

    Dawn of Fliplet Push In menu

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fliplet-menu-push-in
+# Fliplet Push In Menu
+
+The toggle of your menu must be declared using `data-fl-toggle-menu="jquerySelector"` attribute. The jQuery selector will get its `"active"` class toggled on and off when the toggle is clicked/tapped.
+
+You should also have at least one html tag with the `data-fl-navigate-back` attribute to let the user navigate back.
---
+++ js/menu.js
@@ -0,0 +1,13 @@
+$('[data-fl-toggle-menu]').on('click', function() {
+  $('body').addClass('has-push-menu');
+});
+
+$('.fl-menu-overlay').click(function () {
+  $(this).closest('.fl-menu').removeClass('active');
+  $('body').removeClass('has-push-menu');
+});
+
+$('.fl-menu .fl-close-menu').on('click', function() {
+  $(this).parents('.fl-menu').removeClass('active');
+  $('body').removeClass('has-push-menu');
+});

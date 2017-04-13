ce7f381e27a8008ba0e0acf543febeb73ef15430
Hugo Carneiro <hcarneiro@weboo.biz>
2017-04-13 12:47:51 +0100

    Fix rendering issue on iOS

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,16 @@
 $('[data-fl-toggle-menu]').on('click', function() {
   $('body').addClass('has-push-menu');
+  $('html').addClass('has-push-menu');
 });
 
-$('.fl-menu-overlay').click(function () {
+$('.fl-menu-overlay').click(function() {
   $(this).closest('.fl-menu').removeClass('active');
   $('body').removeClass('has-push-menu');
+  $('html').removeClass('has-push-menu');
 });
 
 $('.fl-menu .fl-close-menu').on('click', function() {
   $(this).parents('.fl-menu').removeClass('active');
   $('body').removeClass('has-push-menu');
+  $('html').removeClass('has-push-menu');
 });

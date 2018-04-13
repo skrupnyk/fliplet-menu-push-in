98f3f196f4c4b51b8b0806b3033b53a3d91db2a7
hcarneiro <hcarneiro@weboo.biz>
2018-04-13 10:15:40 +0100

    Create option to enable swipe to go back

--- js/menu.js
+++ js/menu.js
@@ -56,7 +56,9 @@ function init() {
     });
   });
 
-  $('body').hammer().bind('swiperight', function() {
-    Fliplet.Navigate.back();
-  });
+  if (data.swipeBack) {
+    $('body').hammer().bind('swiperight', function() {
+      Fliplet.Navigate.back();
+    });
+  }
 }
--- js/settings.js
+++ js/settings.js
@@ -13,11 +13,19 @@ if (!data.hide) {
   $('input[name="hide-menu"][value="yes"]').prop('checked', true);
 }
 
+if (!data.swipeBack) {
+  $('input[name="swipe-back"][value="no"]').prop('checked', true);
+} else {
+  $('input[name="swipe-back"][value="yes"]').prop('checked', true);
+}
+
 Fliplet.Widget.onSaveRequest(function() {
   var location;
   var hide;
+  var swipeBack;
   var menuLocation = $('#menu-location').val();
   var hideMenu = $('input[name="hide-menu"]:checked').val();
+  var swipe = $('input[name="swipe-back"]:checked').val();
   
   if (menuLocation === 'menuLeft') {
     location = true;
@@ -31,9 +39,16 @@ Fliplet.Widget.onSaveRequest(function() {
     hide = false;
   }
 
+  if (swipe === 'yes') {
+    swipeBack = true;
+  } else {
+    swipeBack = false;
+  }
+
   Fliplet.Widget.save({
     location: location,
-    hide: hide
+    hide: hide,
+    swipeBack: swipeBack
   }).then(function() {
     Fliplet.Widget.complete();
   });

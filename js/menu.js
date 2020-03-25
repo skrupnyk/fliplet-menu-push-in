ba5a3fa75571bbd2cbb65c32c06dc1607729ba4f
Yaroslav Ovdii <yaroslav.o@upplabs.com>
2020-03-25 09:59:12 +0200

    added fixed width class to close icon

--- js/menu.js
+++ js/menu.js
@@ -13,7 +13,7 @@ function init() {
     var $exitButton = $([
       '<li class="linked with-icon" data-fl-exit-app>',
         '<div class="fl-menu-icon">',
-          '<i class="fa fa-sign-out"></i>',
+          '<i class="fa fa-fw fa-sign-out"></i>',
         '</div>',
         '<i class="fa fa-angle-right linked-icon" aria-hidden="true"></i>',
         '<span class="internal-link buttonControl">Exit</span>',
